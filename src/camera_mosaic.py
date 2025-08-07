5e8b433b4b2eff6cd631875e3ae17b4a12adbd23
hyoon122 <hoyoonjo6@gmail.com>
2025-08-07 15:44:49 +0900

    카메라 세팅

--- src/camera_mosaic.py
+++ src/camera_mosaic.py
@@ -8,3 +8,13 @@ MOSAIC_RATE = 15
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 
+# 웹캠 열기
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("카메라에서 영상을 가져올 수 없습니다.")
+        break
+
+    
